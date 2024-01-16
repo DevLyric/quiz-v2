deaf165ca75300e52d4c5d935f295466b66063e2
Douglas Souza <lyricsan@hotmail.com>
2024-01-16 14:35:28 -0300

    working on login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: FormEvent) => event.preventDefault();
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="container mx-auto w-full max-w-lg px-5">
+        <div className="mb-8">
+          <h1 className="text-2xl font-medium">Account Login</h1>
+          <p className="text-sm pt-2 text-gray-400">
+            If you are already a member you can login with your email address
+            and password.
+          </p>
+        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            type="email"
+            className="border rounded p-3"
+            value={email}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setEmail(event.target.value)
+            }
+            placeholder="Email..."
+          />
+
+          <input
+            type="password"
+            className="border rounded p-3"
+            value={password}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setPassword(event.target.value)
+            }
+            placeholder="Password"
+          />
+
+          <button className="rounded p-3  bg-blue-500 text-white font-medium">
+            Login
+          </button>
+
+          <p className="text-center">
+            Don't have an account?{" "}
+            <Link to="/" className="text-blue-700">
+              Sign up here
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
