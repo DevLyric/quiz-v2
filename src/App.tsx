770c148777efaead5791d857263ecc1a34c53204
Douglas Souza <lyricsan@hotmail.com>
2024-01-16 20:00:18 -0300

    working on quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
 import Signup from "./pages/Signup";
+import Quiz from "./pages/Quiz";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </Router>
   );
